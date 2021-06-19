cbe090e49f4dcc5c8ed27e3c9896249c1bdde647
alejandrohl5 <81606955+alejandrohl5@users.noreply.github.com>
2021-06-18 20:27:12 -0500

    victor

--- wireframe/javascript/paginaPrincipal.js
+++ wireframe/javascript/paginaPrincipal.js
@@ -1,1 +1,64 @@
- 
+ function index()
+ {
+    Swal.fire({
+        title:'POLITICA DE PRIVACIDAD',
+         //text:
+        html:'<div class="html"><br><b> Al utilizar nuestros servicios, nos confías tus datos. Entendemos que es una gran responsabilidad y nos esforzamos al máximo para proteger tu información y permitirte controlarla.</b>',
+        // icon:
+        confirmButtonText:"ACEPTO",
+        // footer:
+        width:'50%',
+        //padding:'8cm',
+        // background:
+        // grow:
+        backdrop:true,
+        // timer:
+        // timerProgressBar:
+        // toast:
+         position:'center',
+        allowOutsideClick:false,
+        allowEscapeKey:false,
+        allowEnterKey:false,
+        stopKeydownPropagation:false,
+    
+        // input:
+        // inputPlaceholder:
+        // inputValue:
+        // inputOptions:
+        
+        //  customClass:
+        // 	container:
+        // 	popup:
+        // 	header:
+        // 	title:
+        // 	closeButton:
+        // 	icon:
+        // 	image:
+        // 	content:
+        // 	input:
+        // 	actions:
+        //  confirmButton:"ok",
+        // cancelButton:"cancel",
+        // 	footer:	
+    
+        // showConfirmButton:
+        // confirmButtonColor:
+        // confirmButtonAriaLabel:
+    
+        // showCancelButton:
+        // cancelButtonText:
+        // cancelButtonColor:
+        // cancelButtonAriaLabel:
+        
+        // buttonsStyling:
+        // showCloseButton:
+        // closeButtonAriaLabel:
+    
+    
+        // imageUrl:
+        // imageWidth:
+        // imageHeight:
+        // imageAlt:
+    });
+    
+ }
